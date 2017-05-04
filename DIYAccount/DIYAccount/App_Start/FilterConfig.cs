2d173118c30fb04c82c07974314fc6d56871bfb2
tsengeagle <tsengeagle@gmail.com>
2017-05-04 16:38:08 +0800

    全自製

---
+++ DIYAccount/DIYAccount/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace DIYAccount
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
